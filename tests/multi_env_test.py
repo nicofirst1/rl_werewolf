c48fe43440128f5fbb922f6c64c0c11eb243e71b
dizzi <nicofirst1@gmail.com>
2019-12-18 20:07:11 +0100

    removed print

--- tests/multi_env_test.py
+++ tests/multi_env_test.py
@@ -7,6 +7,5 @@ env = ComMaWw(num_players)
 for i in range(1000):
     actions={id:env.action_space.sample() for id in range(num_players)}
     obs, rewards, dones, info=env.step(actions) # take a random action
-    print(i)
 
 env.close()
