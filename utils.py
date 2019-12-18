db0b3917b465a9bd809d4ba8b5683dfb7b2c560d
dizzi <nicofirst1@gmail.com>
2019-12-18 19:44:59 +0100

    add pprint

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import logging
+
+import numpy as np
+
 
 def str_id_map(str_list):
     """
@@ -18,14 +22,16 @@ def str_id_map(str_list):
 
 def most_frequent(choices):
     """
-    Return most frequent elem in a list, used for votes
+    Return most frequent elem in a object, used for votes
     :param choices: list, list of ints
     :param choices: dict, map agent_id:choice
     :return: int, most common
     """
 
     if isinstance(choices,dict):
-        choices=[v for v in choices.values()]
+            choices=[v for v in choices.values()]
+            if any(isinstance(elem,np.ndarray) for elem in choices):
+                choices=[item for sublist in choices for item in sublist]
 
     counter = 0
     num = choices[0]
@@ -35,7 +41,7 @@ def most_frequent(choices):
             counter = curr_frequency
             num = i
 
-    return num
+    return int(num)
 
 
 def suicide_num(votes):
@@ -44,5 +50,32 @@ def suicide_num(votes):
     :param votes: dict, maps agent id to target
     :return: int
     """
+    res=0
+    for id,trg in votes.items():
 
-    return sum([1 for a,t in votes.items() if a==t])
+        if isinstance(trg,list):
+            res+=1 if id in trg else 0
+        else:
+            res+= 1 if id==trg else 0
+
+    return res
+
+
+def pprint(votes, roles, logger, level=logging.DEBUG):
+
+    assert len(votes)==len(roles), "Len for votes and roles must be the same"
+
+    separator="{:<8} "*len(votes)
+
+    to_print="\n{:<15} {:<15} "+separator
+    to_format=["Role",'Voter/Target' ]+[f"Ag_{id}" for id in votes.keys()]
+    to_print=to_print.format(*to_format)+"\n"
+
+    for idx in range(len(roles)):
+        targets=[f"Ag_{id}" for id in votes[idx]]
+        role=roles[idx]
+        name=f"Ag_{idx}"
+        fr="{:<15} {:<15} "+separator
+        to_print+=fr.format(role,name,*targets)+"\n"
+
+    logger.log(level,to_print)
