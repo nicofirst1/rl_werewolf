c28d78d5f14808b3f29c06375574db971e0a2e1c
dizzi <nicofirst1@gmail.com>
2019-12-19 14:50:39 +0100

    add env reset

--- tests/multi_env.py
+++ tests/multi_env.py
@@ -2,10 +2,12 @@ import gym
 
 from gym_ww.envs import ComMaWw
 
-num_players=5
+num_players=10
 env = ComMaWw(num_players)
 for i in range(1000):
     actions={id:env.action_space.sample() for id in range(num_players)}
     obs, rewards, dones, info=env.step(actions) # take a random action
+    if dones["__all__"]:
+        env.reset()
 
 env.close()
