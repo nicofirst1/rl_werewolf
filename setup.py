4a9cfdc01b7c1138805f3a33bf9c240f1a1ed7df
dizzi <nicofirst1@gmail.com>
2019-12-19 15:19:05 +0100

    updated setup with requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
+from pipreqs.pipreqs import parse_requirements
 from setuptools import setup
+
+
+#parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements("requirements.txt")
+reqs = [str(ir.req) for ir in install_reqs]
+
 
 setup(name='gym_ww',
       version='0.0.1',
-      install_requires=['gym']
+      install_requires=reqs
 )
