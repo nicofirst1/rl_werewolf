2cd093756f5e23ea0a1a089a3da9943161ff0cfc
dizzi <nicofirst1@gmail.com>
2019-12-30 18:27:09 +0100

    moved requirements in setup

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup
 
+
+requires = [
+      "numpy >= 1.17.4",
+      "gym >= 0.15.4",
+      "GPUtil >= 1.4.0",
+
+]
+
 setup(name='gym_ww',
       version='0.0.1',
-      install_requires=['gym']
+      install_requires=requires,
 )
