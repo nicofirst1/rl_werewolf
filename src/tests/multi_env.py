70413bf06ba05a2f607a0564c65ec096409d1102
dizzi <nicofirst1@gmail.com>
2020-01-04 17:44:19 +0100

    add agents key from reset

--- src/tests/multi_env.py
+++ src/tests/multi_env.py
@@ -1,11 +1,13 @@
 import gym
+import tqdm as tqdm
 
 from gym_ww.envs import PolicyWw
 
 num_players=10
 env = PolicyWw(num_players)
-for i in range(1000):
-    actions={id:env.action_space.sample() for id in range(num_players)}
+agent_ids=env.reset().keys()
+for i in tqdm.tqdm(range(1000)):
+    actions={id:env.action_space.sample() for id in agent_ids}
     obs, rewards, dones, info=env.step(actions) # take a random action
     if dones["__all__"]:
         env.reset()
