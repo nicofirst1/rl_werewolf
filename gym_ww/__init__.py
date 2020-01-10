3a50c0bf38ed74f5f5e677ee2aa8be1e70f90e13
dizzi <nicofirst1@gmail.com>
2020-01-10 17:13:56 +0100

    add file handler

--- gym_ww/__init__.py
+++ gym_ww/__init__.py
@@ -4,6 +4,7 @@ import logging
 from ray.rllib.models import ModelCatalog
 
 from WwPreprocessor import WwPreprocessor
+from utils import Params
 
 ww = "werewolf"
 vil = "villager"
@@ -47,5 +48,11 @@ logging.basicConfig(
 )
 logger = logging.getLogger("WwEnvs")
 logger.setLevel(logging.WARN)
+
+# adding file handler
+f_handler = logging.FileHandler(Params.log_match_file)
+f_handler.setLevel(logging.DEBUG)
+logger.addHandler(f_handler)
+
 
 logger.debug("Logger initialized")
