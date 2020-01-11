1b3110184e6b4febdb690171cd7ec2f60db554a7
dizzi <nicofirst1@gmail.com>
2020-01-11 16:01:45 +0100

    update

--- src/tests/multi_env.py
+++ src/tests/multi_env.py
@@ -3,12 +3,17 @@ import tqdm as tqdm
 
 from gym_ww.envs import TurnEnvWw
 
-num_players=10
-env = TurnEnvWw(num_players)
-agent_ids=env.reset().keys()
-for i in tqdm.tqdm(range(1000)):
-    actions={id:env.action_space.sample() for id in agent_ids}
-    obs, rewards, dones, info=env.step(actions) # take a random action
-    if dones["__all__"]:
-        env.reset()
+
+if __name__ == '__main__':
+
+
+    env_configs = {'num_players': 10, "use_act_box": True}
+
+    env = TurnEnvWw(env_configs)
+    agent_ids = env.reset().keys()
+    for i in tqdm.tqdm(range(100)):
+        actions = {id: env.action_space.sample() for id in agent_ids}
+        obs, rewards, dones, info = env.step(actions)  # take a random action
+        if dones["__all__"]:
+            env.reset()
 
