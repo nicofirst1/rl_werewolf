bac7410acb7060a44132176e3da2b9aa86ccef9a
dizzi <nicofirst1@gmail.com>
2020-05-13 19:20:52 +0200

    renamed

---
+++ src/policies/RandomTarget.py
@@ -0,0 +1,25 @@
+from ray.rllib import Policy
+
+class TargetRandom(Policy):
+    """Hand-coded policy that returns random actions."""
+
+    def compute_actions(self,
+                        obs_batch,
+                        state_batches=None,
+                        prev_action_batch=None,
+                        prev_reward_batch=None,
+                        info_batch=None,
+                        episodes=None,
+                        **kwargs):
+        """Compute actions on a batch of observations."""
+        return [self.action_space.sample() for _ in obs_batch], [], {}
+
+    def get_weights(self):
+        return None
+
+    def set_weights(self, weights):
+        pass
+
+    def learn_on_batch(self, samples):
+        """No learning."""
+        return {}
