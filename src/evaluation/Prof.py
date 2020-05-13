828375954784fecbdcb6015d6834b5dd478b763a
dizzi <nicofirst1@gmail.com>
2020-05-13 17:18:34 +0200

    add first row comparison

--- src/evaluation/Prof.py
+++ src/evaluation/Prof.py
@@ -1,6 +1,6 @@
 from utils import Params
-
-from utils.serialization import dump_pkl
+import numpy as np
+from utils.serialization import dump_pkl, load_pkl
 
 
 class Prof:
@@ -9,9 +9,14 @@ class Prof:
     in more episodes
     """
 
-    def __init__(self):
+    def __init__(self, episode_file=None):
+
         # dictionary mapping episode instance to an episode class
-        self.episodes = dict()
+
+        if episode_file is not None:
+            self.episodes = load_pkl(episode_file)
+        else:
+            self.episodes = dict()
         self.log_step = Params.log_step
 
     def add_episode(self, episode_count, episode):
@@ -24,3 +29,17 @@ class Prof:
         # compute complete info for episodes
         self.episodes[episode_count] = episode
         dump_pkl(self.episodes, Params.episode_file)
+
+    def compare_first_targets(self, episode_range:list ):
+
+        f,l=episode_range
+        rg=list(self.episodes)[f:l]
+        f=rg[0]
+        l=rg[-1]
+
+        eps=[v for k,v in self.episodes.items() if f<=k<=l]
+        trg=[ep.targets[0] for ep in eps]
+
+        return np.stack(trg)
+
+
