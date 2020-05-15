3b43f95a183269ad102f8a9d63f7120387fb01d1
dizzi <nicofirst1@gmail.com>
2020-05-15 09:28:06 +0200

    fixed multi agent

--- src/trainable/old/multi_agent.py
+++ src/trainable/old/multi_agent.py
@@ -8,29 +8,42 @@ from wrappers import EvaluationWrapper
 if __name__ == '__main__':
 
     # initialize environment
-    env_configs = {'num_players': 9}
+    env_configs = {'num_players': 20}
     env = EvaluationWrapper(env_configs)
     # get agent ids
     agent_ids = env.reset().keys()
 
+    # variables
+    ww_unite=True # false if ww can target each other during day
     metrics = {k: [] for k in env.custom_metrics.keys()}
     eps = 10000
-    pbar = tqdm(total=eps)
-    # perform 10000 episodes
+    pbar = tqdm(total=eps) # to get counter
+
     ep = 0
     while ep < eps:
 
+        actions={}
         # filter out dead players
         valid_ids = np.where(np.array(env.status_map) == 1)[0]
 
-        # if night filter out wolves
-        if env.phase in [0, 1]:
-            ww_idx = [k for k, v in env.role_map.items() if v == "werewolf"]
-            for ww_id in ww_idx:
-                valid_ids = np.delete(valid_ids, np.where(valid_ids == ww_id))
+        # get shuffled werewolf ids
+        ww_trg_ids=[env.shuffle_map[id_] for id_ in env.get_ids("werewolf", alive=True)]
+        vill_trg_ids=[env.shuffle_map[id_] for id_ in env.get_ids("villager", alive=True)]
+        all_trg_ids= ww_trg_ids + vill_trg_ids
 
-        # chose a valid action
-        actions = {id: random.choice(valid_ids) for id in agent_ids}
+        # for every agent
+        for id_ in agent_ids:
+            if "werewolf" in id_:
+                # perform ww actions
+                if env.phase in [0, 1] or ww_unite:
+                    actions[id_]=random.choice(vill_trg_ids)
+                else:
+                    actions[id_]=random.choice(all_trg_ids)
+            else:
+                # perform vill actions
+                actions[id_] = random.choice(all_trg_ids)
+
+        #step
         obs, rewards, dones, info = env.step(actions)
         # update agent ids
         agent_ids = obs.keys()
@@ -42,5 +55,6 @@ if __name__ == '__main__':
             ep += 1
             pbar.update(1)
 
+    print()
     for k, v in metrics.items():
         print(f"Mean value of {k} is : {np.mean(v)} +- {np.std(v)}")
