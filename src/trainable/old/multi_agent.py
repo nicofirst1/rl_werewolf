cca080e4d8ca7c38956f252e1db8b71b3325b90a
dizzi <nicofirst1@gmail.com>
2020-05-25 16:24:34 +0200

    update

--- src/trainable/old/multi_agent.py
+++ src/trainable/old/multi_agent.py
@@ -1,10 +1,11 @@
-import math
+import itertools
+import operator as op
 import random
 from functools import reduce
-from itertools import combinations
-import operator as op
+
 import numpy as np
 from tqdm import tqdm
+
 from envs import CONFIGS
 from policies.utils import random_non_wolf, revenge_target
 from wrappers import EvaluationWrapper
@@ -16,46 +17,56 @@ def doublefactorial(n):
     else:
         return n * doublefactorial(n - 2)
 
+
 def comb(n, r):
-    r = min(r, n-r)
-    numer = reduce(op.mul, range(n, n-r, -1), 1)
-    denom = reduce(op.mul, range(1, r+1), 1)
+    r = min(r, n - r)
+    numer = reduce(op.mul, range(n, n - r, -1), 1)
+    denom = reduce(op.mul, range(1, r + 1), 1)
     return numer / denom
 
-if __name__ == '__main__':
 
-    config=CONFIGS
-    config['num_players']=21
-    config['max_days']=10
-    # initialize environment
-    env = EvaluationWrapper(config)
+def theo_win_wolf(env):
+    m = env.num_wolves
+    n = env.num_players
+
+    teoretical_p_win = 1
+
+    for i in range(m + 1):
+        num = comb(m, i) * doublefactorial(n - i)
+        num *= (-1) ** i
+        den = doublefactorial(n) * doublefactorial((n % 2) - i)
+        nd = num / den
+        teoretical_p_win -= nd
+
+    print(f"The theoretical ww win is : {teoretical_p_win}")
+
+
+def experiment(eps, env, is_unite, is_random):
     # get agent ids
     obs = env.reset()
     obs = {k: v['dict_obs'] for k, v in obs.items()}
 
-    # variables
-    ww_unite = True  # false if ww can target each other during day
-    metrics = {k: [] for k in env.custom_metrics.keys()}
-    eps = 1000
-    pbar = tqdm(total=eps)  # to get counter
-    action_space=env.action_space
-    to_kill_list=[]
-    signal_conf=(CONFIGS['signal_length'],CONFIGS['signal_range'])
-
     ep = 0
+    action_space = env.action_space
+    to_kill_list = []
+    metrics = {k: [] for k in env.custom_metrics.keys()}
+
+    signal_conf = (CONFIGS['signal_length'], CONFIGS['signal_range'])
+
+    pbar=tqdm(total=eps)
+
     while ep < eps:
 
         actions = {}
-
-        # filter out dead players
-        valid_ids = np.where(np.array(env.status_map) == 1)[0]
 
         # get all other ids
         all_trg_ids = [env.shuffle_map[id_] for id_ in env.get_ids("all", alive=True)]
 
         # perform all ww actions
-        #ww_actions,to_kill_list = revenge_target(action_space, list(obs.values()),signal_conf,unite=False)
-        ww_actions = random_non_wolf(action_space, list(obs.values()),signal_conf,unite=False)
+        if is_random:
+            ww_actions = random_non_wolf(action_space, list(obs.values()), signal_conf, unite=is_unite)
+        else:
+            ww_actions, to_kill_list = revenge_target(action_space, list(obs.values()), to_kill_list, signal_conf, unite=False)
 
         # for every agent
         for idx, id_ in enumerate(obs.keys()):
@@ -80,22 +91,31 @@ if __name__ == '__main__':
             ep += 1
             pbar.update(1)
 
-    del pbar
-    print()
-    m=env.num_wolves
-    n=env.num_players
+        del pbar
 
-    teoretical_p_win=1
+    return metrics
 
 
-    for i in range(m+1):
-        num=comb(m,i)*doublefactorial(n-i)
-        num*=(-1)**i
-        den=doublefactorial(n)*doublefactorial((n%2)-i)
-        nd=num/den
-        teoretical_p_win-=nd
+def tune(combs, config, eps):
+    metrics = {}
+
+    def name(elem):
+        return f"np.{elem[0]}_unite.{elem[1]}_rand.{elem[2]}"
+
+    for elem in tqdm(combs):
+        np, unite, rand = elem
+        config['num_players'] = np
+        env = EvaluationWrapper(config)
+        metrics[name(elem)] = experiment(eps, env, unite, rand)
+
+    return metrics
 
 
-    print(f"The theoretical ww win is : {teoretical_p_win}")
-    for k, v in metrics.items():
-        print(f"Mean value of {k} is : {np.mean(v)} +- {np.std(v)}")
+if __name__ == '__main__':
+    config = CONFIGS
+    config['max_days'] = 1000
+    eps = 500
+    #np, unite, rand
+    combs = [[5, 10,], [ False], [False]]
+    combs = list(itertools.product(*combs))
+    metrics=tune(combs, config, eps)
