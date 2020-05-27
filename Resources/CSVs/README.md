a3e7778d4b947ef24a12d50a9920bfbab9b55320
dizzi <nicofirst1@gmail.com>
2020-05-27 08:08:34 +0200

    aggregated util scripts

--- Resources/CSVs/README.md
+++ Resources/CSVs/README.md
@@ -2,3 +2,4 @@ This dir contains important data files extracted from the game. In particular"
 - [vill theo](Resources/CSVs/vill_theo_win_unite.csv): contains the winning rates in percentage of the villagers against unite random werewolves
 - [ww_theo](Resources/CSVs/ww_theo_win.csv): contains the winning rates in percentage of the werewolves using the closed formula provided in the paper
 - [ww practical](Resources/CSVs/vww_win_practical_random.csv): contains the winning rates in percentage ww in a practical setting with random non unite policy
+- [revenge ww](Resources/CSVs/revenge_ww.csv): contains the winning rates in percentage of ww using revenge policy
---
+++ src/evaluation/utils/csv_difference.py
@@ -0,0 +1,38 @@
+import csv
+from functools import reduce
+
+import numpy as np
+if __name__ == '__main__':
+    f1="/home/dizzi/Desktop/rl-werewolf/Resources/CSVs/practical_ww_unite.csv"
+    f2="/home/dizzi/Desktop/rl-werewolf/Resources/CSVs/practical_ww_revenge.csv"
+
+    with open(f1, "r+") as f:
+        c = csv.reader(f, delimiter=',')
+        rows1 = list(c)
+
+    with open(f2, "r+") as f:
+        c = csv.reader(f, delimiter=',')
+        rows2 = list(c)
+
+
+    h1=rows1[0]
+    h2=rows2[0]
+
+    idx1 = h1.index("mean")
+    idx2 = h2.index("mean")
+
+    res_dict = {float(k[0]): [float(k[idx1])] for k in rows1[1:]}
+
+    for r in rows2[1:]:
+        k=float(r[0])
+        if k in res_dict.keys():
+            res_dict[k]+=[float(r[idx2])]
+
+    res_dict={k:v for k,v in res_dict.items() if len(v)>1}
+    res_dict=[reduce(lambda x,y: x-y, v)**2 for v in res_dict.values()]
+    mse=reduce(lambda x,y: x+y,res_dict)/len(res_dict)
+
+    print(f"MSE between two csv is {mse} on {len(res_dict)} elements")
+
+
+
