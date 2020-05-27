f0a0cec18be237fe322cab2a5e626b7f5c76ce3b
dizzi <nicofirst1@gmail.com>
2020-05-27 14:22:41 +0200

    fixed logic bug

--- src/policies/RandomTargetUnite.py
+++ src/policies/RandomTargetUnite.py
@@ -3,7 +3,7 @@ from ray.rllib import Policy
 from policies.utils import random_non_wolf
 
 
-class RandomTarget(Policy):
+class RandomTargetUnite(Policy):
     """Hand-coded policy that returns random actions. WW will always return a non ww index."""
 
     def compute_actions(self,
@@ -18,7 +18,7 @@ class RandomTarget(Policy):
         observations = [elem.get('obs', {}) for elem in info_batch]
         signal_conf = self.config['env_config']['signal_length'], self.config['env_config']['signal_range']
 
-        action = random_non_wolf(self.action_space, observations, signal_conf, unite=False)
+        action = random_non_wolf(self.action_space, observations, signal_conf, unite=True)
 
         return action, [], {}
 
