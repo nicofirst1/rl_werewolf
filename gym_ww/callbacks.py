4f7e4854d65817be77e157e3d2203503525cb7ac
dizzi <nicofirst1@gmail.com>
2020-06-18 16:58:03 +0200

    using callback class

--- gym_ww/callbacks.py
+++ gym_ww/callbacks.py
@@ -1,47 +1,55 @@
-def on_episode_end(info):
-    """
-    Callback function to be called at the end of an episode, aggregates custom metrics in the episode dict
-    :param info:
-    :return:
-    """
-    episode = info['episode']
-
-    # keep consistency for different versions
-    try:
-        cm = info['env'].envs[0].custom_metrics
-    except AttributeError:
-        cm = info['env'].envs[0].wrapped.custom_metrics
-
-    for k, v in cm.items():
-        episode.custom_metrics[k] = v
+from ray.rllib.agents.callbacks import DefaultCallbacks
 
 
-def mapping_dynamic(agent_id):
-    if "wolf" in agent_id:
-        return "wolf_p"
-    elif "vil" in agent_id:
-        return "vill_p"
-    else:
-        raise NotImplementedError(f"Policy for role {agent_id} not implemented")
+class CustomCallbacks(DefaultCallbacks):
 
+    def __init__(self, legacy_callbacks_dict=None):
+        super().__init__(legacy_callbacks_dict)
+        self.training_policy = 0
 
-def on_train_result(info):
-    trainer, result=info.values()
-    win_ww = result['custom_metrics']['win_wolf_mean']
-    # training_policy = trainer.config['multiagent']['policies_to_train'][0]
-    mapping = trainer.config['multiagent']['policy_mapping_fn'].__name__
+    def on_episode_end(self, worker, base_env,
+                       policies, episode,
+                       **kwargs):
+        """
+        Callback function to be called at the end of an episode, aggregates custom metrics in the episode dict
+        :param info:
+        :return:
+        """
+        # keep consistency for different versions
+        try:
+            cm = base_env.envs[0].custom_metrics
+        except AttributeError:
+            cm = base_env.envs[0].wrapped.custom_metrics
 
-    # if the ww are loosing
-    if win_ww <= 0.50:
-        # is the start and switch the mapping to the dynamic one
-        if "static" in mapping:
-            trainer.config['multiagent']['policy_mapping_fn'] = mapping_dynamic
+        for k, v in cm.items():
+            episode.custom_metrics[k] = v
 
-        trainer.config['multiagent']['policies_to_train'] = "wolf_p"
+        episode.custom_metrics['training_policy'] = self.training_policy
 
-    # if is the start and the ww are loosing
-    else:
-        trainer.config['multiagent']['policies_to_train'] = "vill_p"
+    @staticmethod
+    def mapping_dynamic(agent_id):
+        if "wolf" in agent_id:
+            return "wolf_p"
+        elif "vil" in agent_id:
+            return "vill_p"
+        else:
+            raise NotImplementedError(f"Policy for role {agent_id} not implemented")
 
-    result['custom_metrics']['training_policy'] = trainer.config['multiagent']['policies_to_train']
+    def on_train_result(self, trainer, result, **kwargs):
+        vill_ww = result['custom_metrics']['win_vill_mean']
+        # training_policy = trainer.config['multiagent']['policies_to_train'][0]
+        mapping = trainer.config['multiagent']['policy_mapping_fn'].__name__
 
+        # if the ww are loosing
+        if vill_ww >= 0.65:
+            # is the start and switch the mapping to the dynamic one
+            if "static" in mapping:
+                trainer.config['multiagent']['policy_mapping_fn'] = self.mapping_dynamic
+
+            trainer.config['multiagent']['policies_to_train'] = "wolf_p"
+            self.training_policy = 1
+
+        # if is the start and the ww are loosing
+        elif vill_ww <= 0.35:
+            trainer.config['multiagent']['policies_to_train'] = "vill_p"
+            self.training_policy = 0
